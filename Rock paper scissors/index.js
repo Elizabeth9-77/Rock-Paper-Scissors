e16d897b77ce300f23ce540281020dd5d0bade6c
Elizabeth Travers <melocus@hotmail.co.uk>
2024-08-14 01:45:53 +0000

    fixed a few bugs and added css and html for win/lose and score

--- Rock paper scissors/index.js
+++ Rock paper scissors/index.js
@@ -9,43 +9,45 @@ const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 let playerScore = 0
 let computerScore = 0
 
-function playGame(playerChoice){
-    const computerChoice = choice[Math.floor(Math.random()*3)];
+function playGame(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+
     let result = "";
 
-    if(playerChoice === computerChoice){
+    if (playerChoice === computerChoice) {
         result = "IT'S A TIE!";
     }
-    else{
-        switch(playeChoice){
+    else {
+        switch (playerChoice) {
             case "rock":
-                result = (computerChoice === "scissors")? "YOU WIN!": "YOU LOOE";
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
                 break;
             case "paper":
-                result = (computerChoice === "rock")? "YOU WIN!" : "YOU LOSE";
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
                 break;
             case "scissors":
-            result = (computerChoice === "paper")? "YOU WIN!" : "YOU LOSE";
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
                 break;
         }
     }
-    playerDisplay.textContent = 'PLAYER: ${playerChoice}';
-    computerDisplay.textContent = 'PLAYER: ${computerChoice}'; 
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `computer: ${computerChoice}`;
     resultDisplay.textContent = result;
 
     resultDisplay.classList.remove("greenText", "redText");
 
-    switch(result){
+    switch (result) {
         case "YOU WIN!":
             resultDisplay.classList.add("greenText");
             playerScore++
             playerScoreDisplay.textContent = playerScore
             break;
-            case "YOU LOSE!":
-                resultDisplay.classList.add("redText");
-                computerScore++
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            computerScore++
             computerScoreDisplay.textContent = computerScore
-                break;   
+            break;
     }
 
 }
